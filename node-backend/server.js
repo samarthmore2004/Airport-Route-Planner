c9059116bfb255bd07b2f03a14370734ebf3ef91
Samarth More <150775739+samarthmore2004@users.noreply.github.com>
2026-03-13 03:08:28 +0530

    Update server.js

--- node-backend/server.js
+++ node-backend/server.js
@@ -256,6 +256,8 @@ if (!flightOptions || flightOptions.length === 0) {
 
 
 
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
